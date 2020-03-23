646ff1153e5d8c9af0a6641ffd81798c6c79f78c
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-03-23 13:09:23 +0100

    ligge til IntiDB.php

---
+++ API/IntiDB.php
@@ -0,0 +1,38 @@
+<?php
+header('Content-Type: application/json');
+$OK = true;
+$db = new mysqli("localhost", "root","","bank");
+if($db->connect_error)
+{
+    $OK=false;
+}
+$fil= file_get_contents('../DAL/Bank.sql');
+
+$res = $db->multi_query($fil);
+if($res)
+{
+    do {
+        if ($result = $db->store_result()) {
+            if($result == false)
+            {
+                $OK = false;
+            }
+            $result->free();
+        }
+        $db->more_results();
+    } while ($db->next_result());
+
+}
+else
+{
+    $OK=false;
+}
+$db->close();
+if($OK)
+{
+    echo json_encode("OK");
+}
+else
+{
+    echo json_encode("Feil");
+}
